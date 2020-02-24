6cb499ff88f0b78bb3dc412ecae90104e26ca603
Kevin Rondeau <1863697@cshawi.ca>
2020-02-24 13:10:15 -0500

    First Commit!

---
+++ intra_models/Customer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string name;
+        private string lastName;
+        private string adress;
+        private string city;
+        private string province;
+        private string postalCode;
+        private string contactInfo;
+        private string picturePath;
+        private string info;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Adress
+        {
+            get => adress;
+            set
+            {
+                adress = value;
+                OnPropertyChanged();
+            }
+        }
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Province
+        {
+            get => province;
+            set
+            {
+                province = value;
+                OnPropertyChanged();
+            }
+        }
+        public string PostalCode
+        {
+            get => postalCode;
+            set
+            {
+                postalCode = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ContactInfo
+        {
+            get => contactInfo;
+            set
+            {
+                contactInfo = value;
+                OnPropertyChanged();
+            }
+        }
+        public string PicturePath
+        {
+            get => picturePath;
+            set
+            {
+                picturePath = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Info
+        {
+            get => info;
+            set
+            {
+                info = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    
+}
